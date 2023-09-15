393038e6479d39aef3e28a6e51ab481b6bab4553
Vadim Savin <savinvadim1312@gmail.com>
2023-09-15 15:35:12 +0100

    Assignment: Orders

--- src/app/(admin)/_layout.tsx
+++ src/app/(admin)/_layout.tsx
@@ -1,15 +1,14 @@
-import React from "react";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
-import { Link, Tabs } from "expo-router";
-import { Pressable } from "react-native";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Link, Tabs } from 'expo-router';
+import { Pressable, useColorScheme } from 'react-native';
 
-import Colors from "@/src/constants/Colors";
-import { useColorScheme } from "react-native";
-import { useClientOnlyValue } from "@/src/components/useClientOnlyValue";
+import Colors from '../../constants/Colors';
 
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+/**
+ * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+ */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>["name"];
+  name: React.ComponentProps<typeof FontAwesome>['name'];
   color: string;
 }) {
   return <FontAwesome size={20} style={{ marginBottom: -3 }} {...props} />;
@@ -22,30 +21,29 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         tabBarActiveTintColor: Colors.light.background,
-        tabBarInactiveTintColor: "gainsboro",
+        tabBarInactiveTintColor: 'gainsboro',
         tabBarStyle: {
           backgroundColor: Colors.light.tint,
         },
-        // Disable the static render of the header on web
-        // to prevent a hydration error in React Navigation v6.
-        headerShown: useClientOnlyValue(false, true),
       }}
     >
       <Tabs.Screen name="index" options={{ href: null }} />
+
       <Tabs.Screen
         name="menu"
         options={{
+          title: 'Menu',
           headerShown: false,
-          title: "Menu",
           tabBarIcon: ({ color }) => (
             <TabBarIcon name="cutlery" color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="two"
+        name="orders"
         options={{
-          title: "Orders",
+          title: 'Orders',
+          headerShown: false,
           tabBarIcon: ({ color }) => <TabBarIcon name="list" color={color} />,
         }}
       />
--- src/components/OrderListItem.tsx
+++ src/components/OrderListItem.tsx
@@ -1,52 +1,51 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import React from "react";
-import { Order } from "../types";
-import { ColorProperties } from "react-native-reanimated/lib/typescript/Colors";
-import { Link, useSegments } from "expo-router";
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import React from 'react';
+import { Order } from '../types';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import dayjs from 'dayjs';
+import { Link, useSegments } from 'expo-router';
 
-interface OrderListItemProps {
+dayjs.extend(relativeTime);
+
+type OrderListItemProps = {
   order: Order;
-}
+};
 
 const OrderListItem = ({ order }: OrderListItemProps) => {
   const segments = useSegments();
-  const destination = `/${segments[0]}/orders/${order.id}`;
 
   return (
-    <Link href={destination as any} asChild>
-      <TouchableOpacity style={styles.container}>
+    <Link href={`/${segments[0]}/orders/${order.id}`} asChild>
+      <Pressable style={styles.container}>
         <View>
           <Text style={styles.title}>Order #{order.id}</Text>
-          <Text style={styles.time}>20 hours ago</Text>
+          <Text style={styles.time}>{dayjs(order.created_at).fromNow()}</Text>
         </View>
+
         <Text style={styles.status}>{order.status}</Text>
-      </TouchableOpacity>
+      </Pressable>
     </Link>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     padding: 10,
-    justifyContent: "space-between",
-    borderRadius: 20,
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginBottom: 5,
+    fontWeight: 'bold',
+    marginVertical: 5,
   },
   time: {
-    fontSize: 14,
-    color: "gray",
+    color: 'gray',
   },
   status: {
-    fontSize: 16,
-    fontWeight: "600",
+    fontWeight: '500',
   },
 });
 
